9773a594fbd4811a8d46a93f3fb911c82788493b
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-02-09 16:07:15 -0500

    Font changer started

---
+++ HelloDaniel/app/src/main/java/assignment01/csc214/hellodaniel/FontActivity.java
@@ -0,0 +1,13 @@
+package assignment01.csc214.hellodaniel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FontActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_font);
+    }
+}
