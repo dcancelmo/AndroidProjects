fc2563556fd669098cfdcb6b948fd53cc395d614
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-03-21 23:44:37 -0400

    Assignment6 created

---
+++ Homework6_ListRecyclerViews/app/src/androidTest/java/assignment06/csc214/homework6_listrecyclerviews/ExampleInstrumentedTest.java
@@ -0,0 +1,26 @@
+package assignment06.csc214.homework6_listrecyclerviews;
+
+import android.content.Context;
+import android.support.test.InstrumentationRegistry;
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Instrumentation test, which will execute on an Android device.
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+@RunWith(AndroidJUnit4.class)
+public class ExampleInstrumentedTest {
+    @Test
+    public void useAppContext() throws Exception {
+        // Context of the app under test.
+        Context appContext = InstrumentationRegistry.getTargetContext();
+
+        assertEquals("assignment06.csc214.homework6_listrecyclerviews", appContext.getPackageName());
+    }
+}
