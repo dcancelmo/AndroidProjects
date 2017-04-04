7f1e04d7e854648ad54e38ea774901281552315c
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-04 02:12:52 -0400

    Start hw 7

---
+++ Homework7_SQLite/app/src/main/java/assignment07/csc214/homework7_sqlite/MainActivity.java
@@ -0,0 +1,13 @@
+package assignment07.csc214.homework7_sqlite;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
