df4ab7985671f2156223f49bd10f15213e252c2a
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-11 15:46:09 -0400

    Start of homework 8

---
+++ Homework8_sounds/app/src/main/java/homework08/csc214/homework8_sounds/MainActivity.java
@@ -0,0 +1,21 @@
+package homework08.csc214.homework8_sounds;
+
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        FragmentManager mFragManager = getSupportFragmentManager();
+
+        TrackListFragment mFragment = (TrackListFragment) mFragManager.findFragmentById(R.id.frame_layout_main);
+        if(mFragment == null) {
+            mFragment = new TrackListFragment();
+            mFragManager.beginTransaction().add(R.id.frame_layout_main, fragment).commit();
+        }
+    }
+}
