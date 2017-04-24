07eafb8bcf995982aaf62a543dcf3515a5e9b800
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-24 15:57:50 -0400

    Start part 2

---
+++ Homework09_multithreading/app/src/main/java/homework09/csc214/homework09_multithreading/PrimeActivity.java
@@ -0,0 +1,20 @@
+package homework09.csc214.homework09_multithreading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class PrimeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_prime);
+    }
+
+    public void startFindSquareRoot(View view) {
+    }
+
+    public void startFindLargePrime(View view) {
+    }
+}
