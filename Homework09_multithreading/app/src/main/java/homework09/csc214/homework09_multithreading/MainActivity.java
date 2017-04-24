1590bdf6058ec1fe3efc16e5f73cc4a98abb7f1d
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-23 21:38:38 -0400

    Start homework 9

---
+++ Homework09_multithreading/app/src/main/java/homework09/csc214/homework09_multithreading/MainActivity.java
@@ -0,0 +1,17 @@
+package homework09.csc214.homework09_multithreading;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void startPrimeActivity(View view) {
+    }
+}
