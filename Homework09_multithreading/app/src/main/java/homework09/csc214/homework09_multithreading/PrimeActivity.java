5f314f5379d115234bebf058fd28d1f3cac43827
Dan Cancelmo <dcancelm@u.rochester.edu>
2017-04-24 16:06:09 -0400

    checkValid method done

--- Homework09_multithreading/app/src/main/java/homework09/csc214/homework09_multithreading/PrimeActivity.java
+++ Homework09_multithreading/app/src/main/java/homework09/csc214/homework09_multithreading/PrimeActivity.java
@@ -3,8 +3,12 @@ package homework09.csc214.homework09_multithreading;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class PrimeActivity extends AppCompatActivity {
+
+    public long mUserNum;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,8 +17,32 @@ public class PrimeActivity extends AppCompatActivity {
     }
 
     public void startFindSquareRoot(View view) {
+        if (!checkValid(view)) {
+            //Method
+        }
     }
 
     public void startFindLargePrime(View view) {
+        if (!checkValid(view)) {
+            //Method
+        }
+    }
+
+    public boolean checkValid(View view) {
+        EditText mLongEntry = (EditText) findViewById(R.id.long_entry);
+        long mTemp;
+        try {
+            mTemp = Long.valueOf(mLongEntry.getText().toString());
+        } catch(NumberFormatException e) {
+            Toast.makeText(this, R.string.not_a_valid_long, Toast.LENGTH_LONG).show();
+            return false;
+        }
+        if (mTemp >= 2) {
+            mUserNum = mTemp;
+            return true;
+        } else {
+            Toast.makeText(this, R.string.value_greater_than_equal_2, Toast.LENGTH_LONG).show();
+            return false;
+        }
     }
 }
